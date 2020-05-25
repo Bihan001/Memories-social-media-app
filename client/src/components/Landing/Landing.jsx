f8e735b94f24d00fc3a0c6f9c59adbcc655c5480
BIHAN CHAKRABORTY <bhchakraborty001@gmail.com>
2020-05-25 11:07:35 +0530

    update css

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import Signin from './Signin';
-import Signup from './Signup';
-import Signup2 from './Signup2';
-import Signup3 from './Signup3';
-import '../css/Landing.css';
+import React, { useState } from "react";
+import Signin from "./Signin";
+import Signup from "./Signup";
+import Signup2 from "./Signup2";
+import Signup3 from "./Signup3";
+import "../css/Landing.css";
 
 const Landing = () => {
-  const [visibility, setVisibility] = useState('signin');
+  const [visibility, setVisibility] = useState("signin");
   // if (isAuthenticated && visibility !== 'signup' && visibility !== 'signup2' && visibility !== 'signup3') {
   //   return <Redirect to="/dashboard" />;
   // }
@@ -21,22 +21,25 @@ const Landing = () => {
             <div className="land-featurearea">
               <div className="land-meta">
                 <h1>Memories</h1>
-                <p className="pt-1">‘Time moves in one direction, memory in another’ – William Gibson</p>
+                <p className="pt-1">
+                  ‘Time moves in one direction, memory in another’ – William
+                  Gibson
+                </p>
               </div>
             </div>
           </div>
           <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12">
             <div className="login-reg-bg">
-              {visibility === 'signin' ? (
+              {visibility === "signin" ? (
                 <Signin changeVisibility={onChangeVisibility} />
-              ) : visibility === 'signup1' ? (
+              ) : visibility === "signup1" ? (
                 <Signup changeVisibility={onChangeVisibility} />
-              ) : visibility === 'signup2' ? (
+              ) : visibility === "signup2" ? (
                 <Signup2 changeVisibility={onChangeVisibility} />
-              ) : visibility === 'signup3' ? (
+              ) : visibility === "signup3" ? (
                 <Signup3 changeVisibility={onChangeVisibility} />
               ) : (
-                console.log('Invalid visibility')
+                console.log("Invalid visibility")
               )}
             </div>
           </div>
